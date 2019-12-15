be7690de9e0249de63bfca484398fda075e7f5d0
Staf Verhaegen <staf@stafverhaegen.be>
2019-12-15 13:54:18 +0100

    Basic nmigen generator bench for TAP top cell.
    Simple generation with nmigen and yosys as only dependency.

---
+++ test/nmigen/gen/controller/generate.py
@@ -0,0 +1,20 @@
+#!/bin/env python3
+from nmigen import *
+from nmigen.back.verilog import convert
+from nmigen.build import Platform
+
+from c4m.nmigen.jtag import TAP
+
+class DummyPlatform(Platform):
+    resources = []
+    connectors = []
+    required_tools = ["yosys"]
+
+    def toolchain_prepare(self, fragment, name, **kwargs):
+        raise NotImplementedError
+
+tap = TAP(2)
+f = open("top.v", "w")
+f.write(convert(tap, platform=DummyPlatform()))
+f.close()
+
