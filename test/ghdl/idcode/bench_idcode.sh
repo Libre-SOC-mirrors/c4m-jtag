09341bedae2f6ffc9ca1682435df2908f44d5039
Staf Verhaegen <staf@stafverhaegen.be>
2019-12-15 14:52:36 +0100

    Move idcode.vhdl to test/ghdl/idcode

--- test/ghdl/idcode/bench_idcode.sh
+++ test/ghdl/idcode/bench_idcode.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 vhdldir=`realpath ../../../c4m/vhdl/jtag`
-testvhdldir=`realpath ../../rtl/vhdl`
 opts=--std=08
 ghdl -a $opts $vhdldir/c4m_jtag_pkg.vhdl
 ghdl -a $opts $vhdldir/c4m_jtag_tap_fsm.vhdl
@@ -9,5 +8,5 @@ ghdl -a $opts $vhdldir/c4m_jtag_idblock.vhdl
 ghdl -a $opts $vhdldir/c4m_jtag_iocell.vhdl
 ghdl -a $opts $vhdldir/c4m_jtag_ioblock.vhdl
 ghdl -a $opts $vhdldir/c4m_jtag_tap_controller.vhdl
-ghdl -a $opts $testvhdldir/idcode.vhdl
+ghdl -a $opts ./idcode.vhdl
 ghdl -r $opts bench_idcode --wave=bench_idcode.ghw
